87657db1d5df2afc6c2c2c02eb6f1dcda20044e4
darkout4 <37517130+darkout4@users.noreply.github.com>
2023-10-13 07:01:33 +0100

    Add files via upload

--- stunnel5/ssh-ssl.sh
+++ stunnel5/ssh-ssl.sh
@@ -14,16 +14,16 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 # ==================================================
 # Link Hosting Kalian
-akbarvpn=""raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssh
+akbarvpn=""raw.githubusercontent.com/darkout4/Autoscript/main/ssh
 
 # Link Hosting Kalian Untuk Xray
-akbarvpnn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray"
+akbarvpnn="raw.githubusercontent.com/darkout4/Autoscript/main/xray"
 
 # Link Hosting Kalian Untuk Trojan Go
-akbarvpnnn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/trojango"
+akbarvpnnn="raw.githubusercontent.com/darkout4/Autoscript/main/trojango"
 
 # Link Hosting Kalian Untuk Stunnel5
-akbarvpnnnn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/stunnel5"
+akbarvpnnnn="raw.githubusercontent.com/darkout4/Autoscript/main/stunnel5"
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
