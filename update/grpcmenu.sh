23dac7b1192f7f63343b8b3a130425e025185aa3
darkout4 <37517130+darkout4@users.noreply.github.com>
2023-10-15 09:51:20 +0200

    Add files via upload

--- update/grpcmenu.sh
+++ update/grpcmenu.sh
@@ -7,15 +7,15 @@ yl="\033[0;1;33m"
 wh="\033[0m"
 echo -e "$y                          XRAY/VMESS-GRPC/VLESS-GRPC $wh"
 echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y. Create Account XRAY/VMESS-GRPC/VLESS-GRPC $wh"
-echo -e "$yy 2$y. Delete Account XRAY/VMESS-GRPC/VLESS-GRPC $wh"
-echo -e "$yy 3$y. Extending Account XRAY/VMESS-GRPC/VLESS-GRPC Active Life$wh"
-echo -e "$yy 4$y. Check User Login XRAY/VMESS-GRPC/VLESS-GRPC $wh"
-echo -e "$yy 5$y. Script Update XRAY/VMESS-GRPC/VLESS-GRPC $wh"
+echo -e "$yy 1$y. Criar conta XRAY/VMESS-GRPC/VLESS-GRPC $wh"
+echo -e "$yy 2$y. Excluir conta XRAY/VMESS-GRPC/VLESS-GRPC $wh"
+echo -e "$yy 3$y. Prolongar conta XRAY/VMESS-GRPC/VLESS-GRPC$wh"
+echo -e "$yy 4$y. Verifique login XRAY/VMESS-GRPC/VLESS-GRPC $wh"
+echo -e "$yy 5$y. Atualizar script XRAY/VMESS-GRPC/VLESS-GRPC $wh"
 echo -e "$yy 6$y. Menu$wh"
-echo -e "$yy 7$y. Exit$wh"
+echo -e "$yy 7$y. Sair$wh"
 echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 7 ] : " menu
+read -p "Selecione nas opções [ 1 - 7 ] : " menu
 echo -e ""
 case $menu in
 1)
